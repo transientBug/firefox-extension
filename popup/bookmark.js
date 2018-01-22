580a3dae6e2c02070b1c6fef4449e3915890b11a
JoshAshby <joshuaashby@joshashby.com>
2018-01-21 18:44:37 -0700

    some console logging, and match new API requirements

--- popup/bookmark.js
+++ popup/bookmark.js
@@ -22,28 +22,39 @@ function onGetStorage(result) {
 }
 
 function requestLogin() {
+  console.log("Unauthed, requesting login with a button. yolo.")
   document.querySelector("#login-content").classList.remove("hidden")
 }
 
 function onLoginOpened(tab) {
-  console.log("Goodluck")
+  console.log("Starting auth process, goodluck")
   document.querySelector("#login-content").classList.add("hidden")
 }
 
 function startBookmarking() {
+  console.log("Starting the bookmarking process")
   document.querySelector("#loading-content").classList.remove("hidden")
   browser.tabs.query({active: true, currentWindow: true}).then(bookmarkTab)
 }
 
 function bookmarkTab(tabs) {
   let tab = tabs[0]
-  console.log("Bookmarking tab", tab)
+  console.log("Sending bookmark request for active tab", tab)
 
   let url = `http://localhost:3000/api/v1/bookmarks?auth_token=${this.result.email}:${this.result.apitoken}`
-  let data = { url: tab.url, title: tab.title }
+  let data = {
+    data: {
+      type: "bookmark",
+      attributes: {
+        url: tab.url,
+        title: tab.title
+      }
+    }
+  }
 
   let headers = new Headers({
-    "Content-Type": "application/json"
+    "Content-Type": "application/vnd.api+json",
+    "Accept": "application/vnd.api+json"
   })
 
   let fetchParams = {
@@ -56,15 +67,15 @@ function bookmarkTab(tabs) {
 }
 
 function onResponse(response) {
+  console.log("Got response back from server", response)
+
   if(!response.ok) {
-    debugger
-    throw new TypeError(`Non-Okay response back from the server: ${response.status} - ${response.body}`)
+    throw new TypeError(`Non-Okay response back from the server: ${response.status}`)
   }
 
   let json = response.json()
 
-  console.log("Success:", json)
-
+  console.log("Response was a success", json)
   document.querySelector("#loading-content").classList.add("hidden")
   document.querySelector("#popup-content").classList.remove("hidden")
 }
