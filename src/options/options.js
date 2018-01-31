0079841083f65ac273490abdb8064dc012be0fb2
JoshAshby <joshuaashby@joshashby.com>
2018-01-30 21:04:25 -0700

    small minor cleanup to options page

--- src/options/options.js
+++ src/options/options.js
@@ -1,18 +1,25 @@
-const _form = document.querySelector("form")
 const _devFields = document.getElementById("dev-only")
 
 const _emailField = document.getElementById("email")
 const _apitokenField = document.getElementById("apitoken")
+
 const _endpointField = document.getElementById("endpoint")
 
-function saveOptions(e) {
+async function saveOptions(e) {
   e.preventDefault()
+
+  const result = await browser.storage.local.get(["env"])
 
   browser.storage.local.set({
     email: _emailField.value,
-    apitoken: _apitokenField.value,
-    endpoint: _endpointField.value
+    apitoken: _apitokenField.value
   })
+
+  if(result.env === "DEV") {
+    browser.storage.local.set({
+      endpoint: _endpointField.value
+    })
+  }
 }
 
 async function restoreOptions() {
@@ -20,11 +27,16 @@ async function restoreOptions() {
 
   _emailField.value = result.email
   _apitokenField.value = result.apitoken
-  _endpointField.value = result.endpoint
 
-  if(result.env === "DEV")
+  _emailField.addEventListener("blur", saveOptions)
+  _apitokenField.addEventListener("blur", saveOptions)
+
+  if(result.env === "DEV") {
     _devFields.classList.remove("hidden")
+
+    _endpointField.value = result.endpoint
+    _endpointField.addEventListener("blur", saveOptions)
+  }
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions)
-_form.addEventListener("submit", saveOptions)
