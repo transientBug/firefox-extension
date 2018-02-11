34e770e0b6dd8b8cd22149cac034b2e9dc1e84b7
JoshAshby <joshuaashby@joshashby.com>
2018-02-11 15:13:50 -0700

    small bugfix and version bump for a problem with overwriting the app id

--- src/background/install.js
+++ src/background/install.js
@@ -1,27 +1,27 @@
-const APP_ID = "cad18a8d91f3675493058223c0dbf3905de1d8c38b23fad22391f53156d44827"
+const APP_ID = ""
 
 // Handle setting the environment up based off of if we're installed in
 // temporary mode or node
 async function afterInstall(details) {
   //console.log("Extension installed", details)
-  let data = {
+  const data = {
     appid: APP_ID
   }
 
   if(details.temporary) {
-    data = {
+    Object.assign(data, {
       env: "DEV",
       endpoint: "http://localhost:3000",
       access_token: ""
-    }
+    })
 
     //const redirectURL = browser.identity.getRedirectURL()
     //console.log(`Redirect URL ${redirectURL}`)
   } else {
-    data = {
+    Object.assign(data, {
       env: "PROD",
       endpoint: "https://transientbug.ninja"
-    }
+    })
   }
 
   browser.storage.local.set(data)
